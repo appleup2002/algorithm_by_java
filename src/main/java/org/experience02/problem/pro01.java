8bd117d5b8b02ffa59ef72e668006456768ed309
up apple <1783524093@qq.com>
2024-04-16 19:59:31 +0800

    experince02

---
+++ src/main/java/org/experience02/problem/pro01.java
@@ -0,0 +1,15 @@
+package org.experience02.problem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class pro01 {
+    public static void main(String[] args) {
+
+        List<Integer> position = new ArrayList<>(List.of(100,200,400,600,800,1000,1200,1400,1600,1800,2000,2200));
+        int postMount = 0, postposition = -1;
+        for(int i = 0; i < position.size(); i++){
+
+        }
+    }
+}
